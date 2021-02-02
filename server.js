57558005b79315013a7fb99e93bcc3f78fadeae8
MonilGoklani <monil2912@gmail.com>
2021-02-01 21:25:00 -0500

    added db

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const app = require('./app')
+const {syncAndSeed} = require('./db')
 
 const port = process.env.PORT || 3000
 
-const init = () => {
+const init = async() => {
 try{
+    await syncAndSeed()
     app.listen(port,()=>console.log('listening on port:',port))
 }catch(ex){
     console.log(ex)
--- test/spec.js
+++ test/spec.js
@@ -1,16 +1,20 @@
 const {expect} = require('chai')
+const { syncAndSeed } = require('../db')
 const app = require('supertest')(require('../app'))
 
-describe('firstTest',()=>{
-    it('one equals one',()=>{
-        expect(1).to.equal(1)
-    })
-})
+describe('Tests',()=>{
+    beforeEach(()=>syncAndSeed())
+    describe('firstTest',()=>{
 
-describe('Routes',()=>{
-    it('shows information about the API',async()=>{
-        const response = await app.get('/');
-        expect(response.status).to.equal(200);
-        expect(response.text).to.include('Hello World')
+        it('one equals one',()=>{
+            expect(1).to.equal(1)
+        })
+    })
+    describe('Routes',()=>{
+        it('shows information about the API',async()=>{
+            const response = await app.get('/');
+            expect(response.status).to.equal(200);
+            expect(response.text).to.include('Hello World')
+        })
     })
 })
